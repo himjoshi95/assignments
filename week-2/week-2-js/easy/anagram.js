8cab5202c1e220e5e6f9ad639ad06a21e3e2f234
himjoshi95 <himanshujoshi1012@gmail.com>
2024-08-13 11:49:21 +0530

    first commit to anagram.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+        // Remove any non-alphabetic characters and convert to lowercase
+        let cleanedStr1 = str1.replace(/[^a-z]/gi, '').toLowerCase();
+        let cleanedStr2 = str2.replace(/[^a-z]/gi, '').toLowerCase();
+    
+        // If the cleaned strings have different lengths, they can't be anagrams
+        if (cleanedStr1.length !== cleanedStr2.length) {
+            return false;
+        }
+    
+        // Sort the characters in each string and compare them
+        let sortedStr1 = cleanedStr1.split('').sort().join('');
+        let sortedStr2 = cleanedStr2.split('').sort().join('');
+    
+        return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
