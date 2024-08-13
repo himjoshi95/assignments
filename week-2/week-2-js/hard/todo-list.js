8bda6b7811241b296055a1e0a00cceff4faa2086
himjoshi95 <himanshujoshi1012@gmail.com>
2024-08-14 00:38:08 +0530

    todo-list.js completed

--- week-2/week-2-js/hard/todo-list.js
+++ week-2/week-2-js/hard/todo-list.js
@@ -11,6 +11,42 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];  // Initialize an empty array to hold the todos
+}
+
+add(todo) {
+    this.todos.push(todo);  // Add the new todo to the end of the array
+}
+
+remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+        this.todos.splice(indexOfTodo, 1);  // Remove the todo at the specified index
+    }
+}
+
+update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+        this.todos[index] = updatedTodo;  // Update the todo at the specified index
+    }
+}
+
+getAll() {
+    return this.todos;  // Return all todos
+}
+
+get(indexOfTodo) {
+  if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo]
+        // return this.todos.find((el,i)=> i===indexOfTodo);  // Return the todo at the specified index
+    } else {
+        return null;  // Return null if the index is invalid
+    }
+}
+
+clear() {
+    this.todos = [];  // Clear all todos
+}
 
 }
 
