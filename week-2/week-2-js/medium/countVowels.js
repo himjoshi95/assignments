a68e39cfd1cb54607cf250b8f2c1b6650689023b
himjoshi95 <himanshujoshi1012@gmail.com>
2024-08-13 17:05:55 +0530

    countVowels.js completed

--- week-2/week-2-js/medium/countVowels.js
+++ week-2/week-2-js/medium/countVowels.js
@@ -6,7 +6,13 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  let count = 0
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) count++;
+  }
+  return count;
 }
 
 module.exports = countVowels;
