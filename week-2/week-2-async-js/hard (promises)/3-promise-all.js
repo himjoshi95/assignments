beba3417a2a9c158cf1571f407c04e4ade96da6d
himjoshi95 <himanshujoshi1012@gmail.com>
2024-08-14 20:21:48 +0530

    3-promise-all.js completed

--- week-2/week-2-async-js/hard (promises)/3-promise-all.js
+++ week-2/week-2-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,29 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve,t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve,t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve,t*1000)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const start = new Date();
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(function () {
+        const end = new Date();
+        return end - start
+    })
 }
 
 module.exports = calculateTime;
